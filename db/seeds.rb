8bf0228c841489bbf23d0aac6835f646e330ae91
Caroline Dana <caroline.m.dana@gmail.com>
2022-02-18 11:36:34 +0100

    solving error

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+Movie.destroy_all
+
+25.times do
+  movie = Movie.new(title: Faker::Dessert.flavor, overview: Faker::GreekPhilosophers.quote, poster_url: 'https://picsum.photos/200/300', rating: rand(0.0..10.0))
+  movie.save
+  puts 'created 1 movie'
+end
